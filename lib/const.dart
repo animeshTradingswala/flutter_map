46ebab6edfce5c30c0b7a4912cf39ab02ca397c9
USER <animesh.banerjee@tradingswala.com>
2021-10-25 03:22:34 -0700

    Initial commit

---
+++ lib/const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyCUab73rvZ2wKwkBrPIcbTdK5XLmj8v7CY';
+}
+
+const red = Colors.red;
+const blue = Colors.blue;
